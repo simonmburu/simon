c0cf1acfe2bf52cf2ff2d4f15f12345bf7586cb0
simonmburu <85396013+simonmburu@users.noreply.github.com>
2021-08-07 14:30:53 +0300

    Add files via upload

---
+++ basic mat.c
@@ -0,0 +1,21 @@
+// product of toe intagers
+#include<stdio.h>
+int main()
+{
+	float i,j,product,sum,div,sup;
+	printf("\nenter value of i");
+	scanf("%f",&i);
+	printf("\nenter value of j");
+	scanf("%f",&j);
+	sum=(i+j);
+	div=(i/j);
+	sup=(i-j);
+	product=(i*j);
+	
+	printf("\nthe product is %f",product);
+	printf("\nthe sumt is %f",sum);
+	printf("\nthe div is %f",div);
+	printf("\nthe sup is %f",sup);
+	
+	return 0;
+}
